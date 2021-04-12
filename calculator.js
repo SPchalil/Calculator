815453b7583fbc363676a31551150d4ad73631ca
SP <74065235+SPchalil@users.noreply.github.com>
2021-04-12 00:05:07 -0230

    format

--- calculator.js
+++ calculator.js
@@ -5,14 +5,14 @@ const app = express();
 //app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.urlencoded({
-  extended: true
+    extended: true
 }));
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", function(req, res){
+app.post("/", function (req, res) {
 
     var num1 = Number(req.body.n1);
     var num2 = Number(req.body.n2);
@@ -20,20 +20,19 @@ app.post("/", function(req, res){
     res.send("The result of the calculation is " + result);
 });
 
-app.get("/bmicalculator", function(req,res){
+app.get("/bmicalculator", function (req, res) {
     res.sendFile(__dirname + "/bmiCalculator.html");
 });
 
-app.post("/bmicalculator", function(req, res){
+app.post("/bmicalculator", function (req, res) {
 
     var weight = parseFloat(req.body.weight);
     var height = parseFloat(req.body.height);
-    var bmi = weight/(height*height);
+    var bmi = weight / (height * height);
     res.send("Your BMI is " + bmi);
 });
 
 
-
-app.listen(3000,function(){
+app.listen(3000, function () {
     console.log("Server is running on port 3000");
 });
